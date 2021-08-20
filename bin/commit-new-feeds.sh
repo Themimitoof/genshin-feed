8649a1423da3502b0973a284b57268148c11f8d2
Michael Vieira <contact@mvieira.fr>
2021-08-21 00:30:40 +0200

    Create a first version of the CD

---
+++ bin/commit-new-feeds.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+if [ "$GERALDO_BOULARDO_TOKEN" == "" ]; then
+    echo "Sorry you're not in a GitHub Actions context, you can't use this script."
+    exit 1
+fi
+
+echo "Setup Git initial config"
+git config set --global.user "garaldo.boulardo"
+git config set --global.email "garaldo.boulardo@themimitoof.fr"
+git remote add githubActions https://git:$GERALDO_BOULARDO_TOKEN@github.com/themimitoof/genshin-feed.git
+
+echo "Commit all new feeds"
+git add feed/*
+git commit -m "Update all feeds"
+
+echo "PUSH DA STUFF!"
+git push -u githubActions master
--- bin/prepare-hugo-build.sh
+++ bin/prepare-hugo-build.sh
@@ -1,10 +1,10 @@
 #!/bin/env bash
 
 echo "Copying last-updates.json file to data folder."
-cp feed/last-updates.json site/data
+cp feed/last-updates.json site/data/
 
 echo "Copying all feeds files to static/feed folder."
-mkdir -p site/static/feed
-cp feed/*.xml site/static/feed
+mkdir -p site/static/feed/
+cp feed/*.xml site/static/feed/
 
 echo "Done and ready for building the website."
